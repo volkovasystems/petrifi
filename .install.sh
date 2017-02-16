ce8e66a1f881f4536a8c6722b96c09bc8ed329b7
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-16 17:37:26 +0800

    Updated dependencies.

---
+++ .install.sh
@@ -0,0 +1,17 @@
+yarn add \
+babel-runtime \
+protype \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+protype \
+zelf \
+--save
