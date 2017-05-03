18d1f434bf2c14f1b97bf537957235428a52047e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 17:52:32 +0800

    Updated test.

--- petrifi-test.js
+++ petrifi-test.js
@@ -1,5 +1,7 @@
+const assert = require( "assert" );
 const petrifi = require( "./petrifi.js" );
 
-console.log( petrifi( "hello", "world", { } ) );
+assert.deepEqual( petrifi( "hello", "world", { } ), { "hello": "world" } );
+assert.deepEqual( petrifi( 1, "world", { } ), { "1": "world" } );
 
-console.log( petrifi( 1, "world", { } ) );
+console.log( "ok" );
