91d0622fb38975c17949f84f6c884bd049ba13b0
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-31 14:56:03 +0800

    Update client/server unit test.

---
+++ petrifi.module.js
@@ -0,0 +1,109 @@
+/*;
+	@module-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"package": "petrifi",
+			"path": "petrifi/petrifi.js",
+			"file": "petrifi.js",
+			"module": "petrifi",
+			"author": "Richeve S. Bebedor",
+			"email": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/petrifi.git",
+			"test": "petrifi-test.js",
+			"global": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		Harden visible property.
+
+		Makes your property-value enumerable, non-configurable and non-writable.
+	@end-module-documentation
+
+	@include:
+		{
+			"falzy": "falzy",
+			"kein": "kein",
+			"protype": "protype",
+			"wichevr": "wichevr",
+			"zelf": "zelf"
+		}
+	@end-include
+*/
+
+const falzy = require( "falzy" );
+const kein = require( "kein" );
+const protype = require( "protype" );
+const wichevr = require( "wichevr" );
+const zelf = require( "zelf" );
+
+const petrifi = function petrifi( property, value, entity ){
+	/*;
+		@meta-configuration:
+			{
+				"property:required": [
+					"number",
+					"string"
+				],
+				"value:required": "*",
+				"entity": "object"
+			}
+		@end-meta-configuration
+	*/
+
+	if( falzy( property ) || !protype( property, NUMBER + STRING ) ){
+		throw new Error( "invalid property" );
+	}
+
+	entity = wichevr( entity, zelf( this ) );
+
+	if( kein( property, entity ) ){
+		return entity;
+	}
+
+	try{
+		Object.defineProperty( entity, property, {
+			"value": value,
+
+			"configurable": false,
+			"enumerable": true,
+			"writable": false
+		} );
+
+	}catch( error ){
+		throw new Error( `cannot petrify ${ property }, ${ error.stack }` );
+	}
+
+	return entity;
+};
+
+module.exports = petrifi;
